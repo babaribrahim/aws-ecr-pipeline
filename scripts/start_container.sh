a396d6573f6715d1278f9e80da0726f180aca84b
babarr1 <i230040@isb.nu.edu.pk>
2026-07-23 05:32:53 -0700

    Add pipeline specs and deployment scripts

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 395063533284.dkr.ecr.us-west-2.amazonaws.com
 
 # Pull the latest image
-docker pull 395063533284.dkr.ecr.us-west-2.amazonaws.com/<my-html-app>:latest
+docker pull 395063533284.dkr.ecr.us-west-2.amazonaws.com/my-html-app:latest
 
 # Run the new container
-docker run -d --name my-app-container -p 80:80 395063533284.dkr.ecr.us-west-2.amazonaws.com/<my-html-app>:latest
+docker run -d --name my-app-container -p 80:80 395063533284.dkr.ecr.us-west-2.amazonaws.com/my-html-app:latest
